5d7f0e8fda2fb80eba90898438098fbedb05bba9
wangzs <qc_8b15f2d4659242afa7bc04e20e94a032@git.code.tencent.com>
2021-05-21 15:20:29 +0800

    refactor: 自定义vue指令生成水印修改

--- src/common/js/waterMark.js
+++ src/common/js/waterMark.js
@@ -1,33 +1,33 @@
 /**
-  *watermark指令
-  *解决:给页面生成水印
-  *基本原理:给选择器添加背景图片
-  *用法: v-watermark=" {options}”
-  *option:
-  *@param {string} text 水印文字
-  *@param {string} width 宽度
-  *@param {string} textColor 文字颜色
-  **/
+ *watermark指令
+ *解决:给页面生成水印
+ *基本原理:给选择器添加背景图片
+ *用法: v-watermark=" {options}”
+ *option:
+ *@param {string} text 水印文字
+ *@param {string} width 宽度
+ *@param {string} textColor 文字颜色
+ **/
 
-export default (Vue)=>{
-  Vue.directive('watermark',(el,binding)=>{
-    function addwaterMarker(str,parentNode,font,textColor){
-      var can = document.createElement('canvas')
-      parentNode.appendchild(can)
-      can.width = 150
-      can.height = 150
-      can.style.display = 'none'
-      var cans = can.getcontext('2d')
+export default (Vue) => {
+  Vue.directive("watermark", (el, binding) => {
+    function addwaterMarker(str, parentNode, font, textColor) {
+      var can = document.createElement("canvas");
+      parentNode.appendChild(can);
+      can.width = 150;
+      can.height = 150;
+      can.style.display = "none";
+      var cans = can.getContext("2d");
       // cans.rotate( -20 * Math.PI / 180)
-      cans.rotate(-45 * Math.PI / 180);
-      cans.font = font || "20px Arial"
-      cans.fillstyle = textColor || 'rgba(153,153,153,0.2)'
+      cans.rotate((-45 * Math.PI) / 180);
+      cans.font = font || "20px Arial";
+      cans.fillStyle = textColor || "rgba(153,153,153,0.2)";
       // cans.textAlign = 'center'
       // cans.textBaseline = 'MiddLe'
-      // cans.filLText(str, can.width / 3, can. height / 2)
-      cans.filLText(str,-70,90,135);
-      parentNode.style.bacgroundImage ='url('+ can.toDataURL('image.png') + ')'
+      // cans.fillText(str, can.width / 3, can. height / 2)
+      cans.fillText(str, -70, 90, 135);
+      parentNode.style.backgroundImage = "url(" + can.toDataURL("image.png") + ")";
     }
-    addwaterMarker(binding.value.text,el,binding.value.font,binding.value.textColor)
-  })
-}
+    addwaterMarker(binding.value.text, el, binding.value.font, binding.value.textColor);
+  });
+};
